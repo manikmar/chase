ea315e4ebd68f143ec1c9d6eede119c9618ecca5
manikmar <manikumar.panithi@gmail.com>
2026-08-29 16:41:06 -0400

    Update TestNoteBook.java

--- src/main/java/com/chase/uitil/TestNoteBook.java
+++ src/main/java/com/chase/uitil/TestNoteBook.java
@@ -5,6 +5,6 @@ public class TestNoteBook {
     public static void main(String[] args) {
         System.out.println("Welcome to the Git Rebase...!");
          System.out.println("Welcome to the Git to get latest changes from dev-162(main) branch...!");
-        
+        System.out.println("test update intellij feature");
     }
 }
