a2ce3886a6d90f55abe746e40fa606d4e798e512
manikmar <manikumar.panithi@gmail.com>
2026-08-29 17:54:50 -0400

    Update TestNoteBook.java
    intellij rebase

--- src/main/java/com/chase/uitil/TestNoteBook.java
+++ src/main/java/com/chase/uitil/TestNoteBook.java
@@ -5,8 +5,7 @@ public class TestNoteBook {
     public static void main(String[] args) {
         System.out.println("Welcome to the Git Rebase...!");
         System.out.println("Welcome to the Git to get latest changes from dev-162(main) branch...!");
-        System.out.println("test update intellij feature");
-
-         System.out.println("intellij pull with rebase option");
+        System.out.println("test merge intellij feature");
+        System.out.println("test rebase intellij feature");
     }
 }
