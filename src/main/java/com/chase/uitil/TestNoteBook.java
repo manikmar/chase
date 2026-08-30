11dc8cf1ff8b73fc74b16c9fc2059c3f8acf1245
Mani <manikumar.panithi@gmail.com>
2026-08-29 22:09:23 -0400

    pull with rebase

--- src/main/java/com/chase/uitil/TestNoteBook.java
+++ src/main/java/com/chase/uitil/TestNoteBook.java
@@ -4,15 +4,6 @@ public class TestNoteBook {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Git Rebase...!");
-        System.out.println("000000000000000000");
-        System.out.println("111111111111111111");
-
-        System.out.println("222222222");
-
-        System.out.println("3333333333");
-
-        System.out.println("44444444");
-
-         System.out.println("55555555555");
+        System.out.println("Find a new Way");
     }
 }
