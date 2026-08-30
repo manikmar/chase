c9ea630cfc705f9612cc8fdf90a26eaaab206778
manikmar <manikumar.panithi@gmail.com>
2026-08-29 22:17:46 -0400

    Update TestNoteBook.java

--- src/main/java/com/chase/uitil/TestNoteBook.java
+++ src/main/java/com/chase/uitil/TestNoteBook.java
@@ -5,5 +5,6 @@ public class TestNoteBook {
     public static void main(String[] args) {
         System.out.println("Welcome to the Git Rebase...!");
         System.out.println("Find a new Way");
+        System.out.println("fallow pull with rebase in the feature branch");
     }
 }
