dcefe4a7bcbbb102bed091b03b77a51541070ed9
manikmar <manikumar.panithi@gmail.com>
2026-08-31 21:57:48 -0400

    Update TestNoteBook.java

--- src/main/java/com/chase/uitil/TestNoteBook.java
+++ src/main/java/com/chase/uitil/TestNoteBook.java
@@ -3,10 +3,6 @@ package com.chase.uitil;
 public class TestNoteBook {
 
     public static void main(String[] args) {
-        System.out.println("from dev 1 ------>");
-        System.out.println("fea ----->");
         
-        System.out.println("test Data");
-        System.out.println("Data From Dev");
     }
 }
